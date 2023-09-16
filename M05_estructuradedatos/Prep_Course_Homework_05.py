e4677e701268d6287d7e717c0336fb58a5c4ff01
Jorge Alejandro Cruz Balcázar <143291619+JorgeCruzBM@users.noreply.github.com>
2023-09-15 20:22:07 -0600

    Actualizar Prep_Course_Homework_05.py

--- M05_estructuradedatos/Prep_Course_Homework_05.py
+++ M05_estructuradedatos/Prep_Course_Homework_05.py
@@ -6,21 +6,22 @@
 # 1) Crear una lista que contenga nombres de ciudades del mundo que contenga más de 5 elementos e imprimir por pantalla
 
 # In[3]:
-
+WC = ["Monaco", "Dublin", "San Cristobal", "Ottawa", "Comitan", "Pujiltik"]
+print (WC)
 
 
 
 # 2) Imprimir por pantalla el segundo elemento de la lista
 
 # In[4]:
-
+print (WC[1])
 
 
 
 # 3) Imprimir por pantalla del segundo al cuarto elemento
 
 # In[8]:
-
+print (WC[1:3])
 
 
 
@@ -28,7 +29,7 @@
 # 4) Visualizar el tipo de dato de la lista
 
 # In[12]:
-
+type(WC)
 
 
 
@@ -36,7 +37,7 @@
 # 5) Visualizar todos los elementos de la lista a partir del tercero de manera genérica, es decir, sin explicitar la posición del último elemento
 
 # In[14]:
-
+print (WC[2:])
 
 
 
@@ -44,7 +45,7 @@
 # 6) Visualizar los primeros 4 elementos de la lista
 
 # In[15]:
-
+print (WC[:3])
 
 
     
@@ -53,7 +54,7 @@
 # 7) Agregar una ciudad más a la lista que ya exista y otra que no ¿Arroja algún tipo de error?
 
 # In[16]:
-
+"Si, por el elemento repetido
 
 
 
@@ -65,7 +66,7 @@
 # 8) Agregar otra ciudad, pero en la cuarta posición
 
 # In[20]:
-
+WC.insert(2, "Konohagakure")
 
 
 
@@ -79,21 +80,21 @@
 
 # In[22]:
 
-
-
+Nar= ["Kamartaj", "Kirigakure", "Marisma Criminal"]
+NL = WC + Nar
 
 # 10) Encontrar el índice de la ciudad que en el punto 7 agregamos duplicada. ¿Se nota alguna particularidad?
 
 # In[23]:
 
-
+#No se agregó
 
 
 
 # 11) ¿Qué pasa si se busca un elemento que no existe?
 
 # In[24]:
-
+#Error de sintaxis
 
 
 
@@ -101,7 +102,7 @@
 # 12) Eliminar un elemento de la lista
 
 # In[25]:
-
+WC.pop()
 
 
 
@@ -110,36 +111,36 @@
 
 # In[27]:
 
-
+Erro
 
 
 
 # 14) Extraer el úlimo elemento de la lista, guardarlo en una variable e imprimirlo
 
 # In[28]:
-
-
+LE=WC.pop()
+print (LE)
 
 
 
 # 15) Mostrar la lista multiplicada por 4
 
 # In[29]:
-
+print (WC*4)
 
 
 
 # 16) Crear una tupla que contenga los números enteros del 1 al 20
 
 # In[32]:
-
+NE=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 
 
 
 # 17) Imprimir desde el índice 10 al 15 de la tupla
 
 # In[35]:
-
+print (NE[10:15])
 
 
 
