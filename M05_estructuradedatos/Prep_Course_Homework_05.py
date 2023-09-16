0f08cc69bf4c14159574a5e687e115c5d7b4ed03
Jorge Alejandro Cruz Balcázar <143291619+JorgeCruzBM@users.noreply.github.com>
2023-09-16 14:19:55 -0600

    Actualizar Prep_Course_Homework_05.py
    d

--- M05_estructuradedatos/Prep_Course_Homework_05.py
+++ M05_estructuradedatos/Prep_Course_Homework_05.py
@@ -147,39 +147,44 @@ print (NE[10:15])
 # 18) Evaluar si los números 20 y 30 están dentro de la tupla
 
 # In[41]:
-
-
+20 in NE
+30 in NE
 
 
 
 # 19) Con la lista creada en el punto 1, validar la existencia del elemento 'París' y si no existe, agregarlo. Utilizar una variable e informar lo sucedido.
 
 # In[48]:
+if "Paris" in WC:
+    print ("Paris is already in WC")
+   
+else:
+	print ("Paris wasn't found in WC. Paris is going to be added")
+	WC.append("Paris")
+    
 
-
-
+print (WC)
 
 
 # 20) Mostrar la cantidad de veces que se encuentra un elemento específico dentro de la tupla y de la lista
 
 # In[51]:
-
-
-
+	NE.count(15)
+WC.count("Paris")
 
 
 # 21) Convertir la tupla en una lista
 
 # In[52]:
 
-
+list(NE)
 
 
 
 # 22) Desempaquetar solo los primeros 3 elementos de la tupla en 3 variables
 
 # In[55]:
-
+Prim, Seg, Ter= NE[:3]
 
 
 
@@ -187,8 +192,10 @@ print (NE[10:15])
 # 23) Crear un diccionario utilizando la lista crada en el punto 1, asignandole la clave "ciudad". Agregar tambien otras claves, como puede ser "Pais" y "Continente".
 
 # In[57]:
-
-
+dicc=dict()
+dicc["Ciudad"]= WC
+dicc[57]=
+dicc[5opper]
 
 
 
@@ -197,13 +204,13 @@ print (NE[10:15])
 
 # In[59]:
 
-
+dicc.keys()
 
 
 # 25) Imprimir las ciudades a través de su clave
 
 # In[61]:
-
+print (dicc[Ciudad])
 
 
 
